9ff5236a6681da3f766bf5fb0cba4f240972d84e
Vera Korzun <veraminich@gmail.com>
2021-01-13 13:30:03 +0100

    Added changes

---
+++ src/data/index.js
@@ -0,0 +1,6 @@
+import user from "./user.json";
+import friends from "./friends.json";
+import statisticalData from "./statistical-data.json";
+import transactions from "./transactions.json";
+
+export { user, friends, statisticalData, transactions };
